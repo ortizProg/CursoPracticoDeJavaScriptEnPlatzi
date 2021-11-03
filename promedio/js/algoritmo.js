5095bedf8d88fb2c9d74f68841d3964c722b449e
ortizProg <santiagoortiz012@gmail.com>
2021-11-03 15:14:34 -0500

    Nuevo proyecto del promedio

---
+++ promedio/js/algoritmo.js
@@ -0,0 +1,15 @@
+const lista = [
+    3,
+    4,
+    1000,
+];
+function calcularPromedio(lista) {
+    const sumaLista = lista.reduce(
+        function(valorAcumulado = 0, nuevoValor) {
+            return valorAcumulado + nuevoValor;
+        }
+    );
+    const promedioLista = sumaLista / lista.length; 
+    
+    console.log("El promedio es: " + promedioLista);
+}
